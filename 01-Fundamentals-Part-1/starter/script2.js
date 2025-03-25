06d27d143c9dc5e372bbc1d3ed90cee44afb6d5f
SyzmonMazurek <91948546+SyzmonMazurek@users.noreply.github.com>
2025-03-25 16:15:13 +0100

    assignment 2

--- 01-Fundamentals-Part-1/starter/script2.js
+++ 01-Fundamentals-Part-1/starter/script2.js
@@ -5,3 +5,10 @@ let population = 38;
 console.log(country);
 console.log(continent);
 console.log(population);
+
+let isIsland = country;
+let language;
+
+console.log(isIsland);
+console.log(language);
+
