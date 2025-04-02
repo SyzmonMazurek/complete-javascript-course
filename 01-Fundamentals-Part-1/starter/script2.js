10bf2bac33790c31111edaa89a19d2b096f1490c
SyzmonMazurek <91948546+SyzmonMazurek@users.noreply.github.com>
2025-04-02 16:11:07 +0200

    assignment 13

--- 01-Fundamentals-Part-1/starter/script2.js
+++ 01-Fundamentals-Part-1/starter/script2.js
@@ -15,5 +15,35 @@ console.log(typeof isIsland);
 console.log(typeof population);
 console.log(typeof country);
 console.log(typeof language);
+
+
+const country = "Poland";
+const continent = "Europe";
+let population = 38;
+const isIsland = false;
+language = "Polish";
 */
 
+const country = "Poland";
+const continent = "Europe";
+let population = 38;
+const isIsland = false;
+language = "Polish";
+
+console.log(population / 2);
+popilation++;
+let finlandPopulation = 6
+console.log(population > finlandPopulation);
+const averagePopulation = 33
+console.log(population < averagePopulation);
+
+const description = 
+country +
+" is in " + 
+continent +
+", and its " +
+population +
+" milion people speak " +
+language;
+
+console.log(description)
