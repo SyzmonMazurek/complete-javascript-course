f14974c0478e8b1a2ea8145e12cc1e619bbe2ad7
SyzmonMazurek <91948546+SyzmonMazurek@users.noreply.github.com>
2025-04-02 14:34:11 +0200

    lesson 13

--- 01-Fundamentals-Part-1/starter/Script.js
+++ 01-Fundamentals-Part-1/starter/Script.js
@@ -3,6 +3,32 @@ let js = 'amazing';
 console.log(40 + 8 + 23 - 10);
 */
 
+/*
 let firstName = "Jonas";
 
 console.log(firstName)
+*/
+
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now -2018;
+console.log(ageJonas, ageSarah);
+
+console.log(ageJonas * 2, ageJonas / 10, 2**3);
+// 2 ** 3 = 2 to the power of 3 = 2 * 2 * 2 = 8
+
+const firstName = "Jonas";
+const lastName = "Schmedtmann";
+console.log(firstName + " " + lastName);
+// " " - create space in 
+
+let x = 10 + 5; // x = 15
+x += 10; // x = x + 10 = 25
+x * 4; // x = 25 * 4 =100
+x++; // add 1 to x
+x--; // deduce 1 from x
+console.log(x);
+
+
+
+
--- 01-Fundamentals-Part-1/starter/script2.js
+++ 01-Fundamentals-Part-1/starter/script2.js
@@ -1,12 +1,12 @@
-
+/*
 let country = "Poland";
 let continent = "Europe";
 let population = 38;
-/*
+
 console.log(country);
 console.log(continent);
 console.log(population);
-*/
+
 
 let isIsland = false;
 let language;
@@ -15,4 +15,5 @@ console.log(typeof isIsland);
 console.log(typeof population);
 console.log(typeof country);
 console.log(typeof language);
+*/
 
